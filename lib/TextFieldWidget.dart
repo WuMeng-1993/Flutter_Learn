629c08aec7ea184ef486f82f474841cade72b80d
WuMeng <541345568@qq.com>
2020-05-08 23:26:06 +0800

    添加TextEditingController

--- lib/TextFieldWidget.dart
+++ lib/TextFieldWidget.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class TextFieldWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TextEditingController _selectionController = new TextEditingController();
+    _selectionController.text = "Hello World!";
+    _selectionController.selection = new TextSelection(
+        baseOffset: 0, extentOffset: _selectionController.text.length);
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("TextField"),
@@ -12,11 +17,21 @@ class TextFieldWidget extends StatelessWidget {
           new TextField(
             autofocus: true,
             decoration: new InputDecoration(
-              labelText: "用户名",
-              hintText: "用户名或邮箱",
-              prefixIcon: new Icon(Icons.person)
-            ),
-          )
+                labelText: "用户名",
+                hintText: "用户名或邮箱",
+                prefixIcon: new Icon(Icons.person)),
+            onChanged: (value) {
+              print(value);
+            },
+            controller: _selectionController,
+          ),
+          new TextField(
+            decoration: new InputDecoration(
+                labelText: "密码",
+                hintText: "您的登录密码",
+                prefixIcon: new Icon(Icons.lock)),
+            obscureText: true,
+          ),
         ],
       ),
     );
