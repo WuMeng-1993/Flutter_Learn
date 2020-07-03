4e25ebe0a5f5d10fbad244574d71f569427f1100
妖颜TMD祸众 <541345568@qq.com>
2020-07-03 10:43:33 +0800

    Update ShareDataWidget.dart
    完善ShareDataWidget

--- lib/ShareDataWidget.dart
+++ lib/ShareDataWidget.dart
@@ -10,6 +10,7 @@ class ShareDataWidget extends InheritedWidget {
     Widget child
   }) : super(child: child);
   
+  // 定义一个便捷方法，方便子树中的Widget获取共享数据
   static ShareDataWidget of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<ShareDataWidget>();
   }
@@ -17,6 +18,8 @@ class ShareDataWidget extends InheritedWidget {
   // 该回调决定当data发生变化时，是否通知子树中依赖data的Widget
   @override
   bool updateShouldNotify(ShareDataWidget oldWidget) {
+    // 如果返回true，则子树中依赖(build函数中有调用)本Widget的
+    // 的子Widget的`state.didChangeDependencies`会被调用
     return oldWidget.data != data;
   }
 
