e290ded693ec86b14672be66725d0b7173869011
妖颜TMD祸众 <541345568@qq.com>
2020-07-03 10:30:00 +0800

    Update ShareDataWidget.dart
    完成ShareDataWidget组件

--- lib/ShareDataWidget.dart
+++ lib/ShareDataWidget.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 
 class ShareDataWidget extends InheritedWidget {
 
+  // 需要在子树中共享的数据，保存点击次数
+  final int data;
+  
   ShareDataWidget({
     @required this.data,
     Widget child
   }) : super(child: child);
+  
+  static ShareDataWidget of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ShareDataWidget>();
+  }
 
-  final int data;
-
+  // 该回调决定当data发生变化时，是否通知子树中依赖data的Widget
   @override
   bool updateShouldNotify(ShareDataWidget oldWidget) {
     return oldWidget.data != data;
