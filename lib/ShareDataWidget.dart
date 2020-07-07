93f100a5479d866c560a027da3d4501bea088822
WuMeng <541345568@qq.com>
2020-07-07 23:06:24 +0800

    修改为不依赖

--- lib/ShareDataWidget.dart
+++ lib/ShareDataWidget.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ShareDataWidget extends InheritedWidget {
-
   // 需要在子树中共享的数据，保存点击次数
   final int data;
-  
-  ShareDataWidget({
-    @required 
-    this.data,
-    Widget child
-  }) : super(child: child);
-  
+
+  ShareDataWidget({@required this.data, Widget child}) : super(child: child);
+
   // 定义一个便捷方法，方便子树中的Widget获取共享数据
   static ShareDataWidget of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<ShareDataWidget>();
+//    return context.dependOnInheritedWidgetOfExactType<ShareDataWidget>();
+    return context
+        .getElementForInheritedWidgetOfExactType<ShareDataWidget>()
+        .widget;
   }
 
   // 该回调决定当data发生变化时，是否通知子树中依赖data的Widget
@@ -23,5 +21,4 @@ class ShareDataWidget extends InheritedWidget {
     // 的子Widget的`state.didChangeDependencies`会被调用
     return oldWidget.data != data;
   }
-
 }
