fa314a7a32ab417adb42a8a0872229c2d30b392d
妖颜TMD祸众 <541345568@qq.com>
2020-07-07 11:28:54 +0800

    Update InheritedWidgetTestRoute.dart
    添加注释

--- lib/InheritedWidgetTestRoute.dart
+++ lib/InheritedWidgetTestRoute.dart
@@ -3,10 +3,12 @@ import 'package:flutterapp1/InheriteTestWidget.dart';
 import 'ShareDataWidget.dart';
 
 class InheritedWidgetTestRoute extends StatefulWidget {
+  
   @override
   State<InheritedWidgetTestRoute> createState() {
     return new _InheritedWidgetTestRoute();
   }
+  
 }
 
 class _InheritedWidgetTestRoute extends State<InheritedWidgetTestRoute> {
@@ -15,11 +17,13 @@ class _InheritedWidgetTestRoute extends State<InheritedWidgetTestRoute> {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         title: Text("数据共享"),
       ),
       body: Center(
+        // 使用ShareDataWidget
         child: ShareDataWidget(
           data: count,
           child: Column(
@@ -27,10 +31,12 @@ class _InheritedWidgetTestRoute extends State<InheritedWidgetTestRoute> {
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.only(bottom: 20),
+                // 子Widget中依赖ShareDataWidget
                 child: InheriteTestWidget(),
               ),
               RaisedButton(
                 child: Text("增加"),
+                // 每点击一次，count自增，然后重新build,ShareDataWidget中的data将被更新
                 onPressed: () => setState(() => ++count),
               )
             ],
@@ -39,4 +45,5 @@ class _InheritedWidgetTestRoute extends State<InheritedWidgetTestRoute> {
       ),
     );
   }
+  
 }
