84aa094d3afccf772e48b2de1c16b10d3cc2da09
WuMeng <541345568@qq.com>
2020-07-22 23:54:21 +0800

    创建DialogTestRoute文件

---
+++ lib/dialog/DialogTestRoute.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DialogTestRoute extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("对话框"),
+      ),
+      body: Center(
+
+      ),
+    );
+  }
+
+
+}
